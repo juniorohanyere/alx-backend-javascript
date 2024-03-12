2ef4ed518ad6edc4ee83441b820976173d51d6bb
Junior Ohanyere <junohanyere@gmail.com>
2024-03-12 15:04:38 +0100

    return the sum of all the student ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,11 +1,11 @@
 /* function that returns the sum of all student ids */
 
 export default function getStudentIdsSum(getListStudents) {
-  const arr = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+  if (getListStudents instanceof Array) {
+    return getListStudents.reduce(
+      (prev, current) => prev.id || prev + current.id, 0,
+    );
+  }
 
-  return arr.reduce(((reducer, arr) => reducer + arr.id), 0);
+  return 0;
 }
